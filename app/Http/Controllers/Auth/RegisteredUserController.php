15d11b702fcbaa0aac05c4dbc0ba1ab4d6a62170
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-10 11:42:10 -0500

    Actualizando el formulario de registro

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\School;
+use App\Models\Student;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -19,7 +21,8 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        $schools = School::all();
+        return view('auth.register', compact('schools'));
     }
 
     /**
@@ -31,14 +34,28 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'regex:/^.*@unasam\.edu\.pe$/', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'dni' => ['required', 'numeric', 'digits:8','unique:students'],
+            'phone' => ['required', 'numeric'],
+            'school_id' => ['required', 'numeric'],
+            'condition' => ['required', 'in:estudiante,egresado'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'last_name' => $request->last_name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+        ]);
+
+        Student::create([
+            'dni' => $request->dni,
+            'phone' => $request->phone,
+            'condition' => $request->condition,
+            'school_id' => $request->school_id,
+            'user_id' => $user->id,
         ]);
 
         event(new Registered($user));
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,18 +2,62 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
+        <!--DNI -->
+        <div >
+            <x-input-label for="dni" :value="__('DNI')" />
+            <x-text-input id="dni" class="block mt-1 w-full" type="number" name="dni" :value="old('dni')" required autofocus autocomplete="dni" />
+            <x-input-error :messages="$errors->get('dni')" class="mt-2" />
+        </div>
+
         <!-- Name -->
-        <div>
+        <div class="mt-4">
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
+        <!--Last Name -->
+        <div class="mt-4">
+            <x-input-label for="last_name" :value="__('Apellidos')" />
+            <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autofocus autocomplete="last_name" />
+            <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
+        </div>
+
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Correo institucional')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!--Phone -->
+        <div class="mt-4">
+            <x-input-label for="phone" :value="__('Celular')" />
+            <x-text-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" required autofocus autocomplete="phone" />
+            <x-input-error :messages="$errors->get('phone')" class="mt-2" />
+        </div>
+
+        <!--School -->
+        <div class="mt-4">
+            <x-input-label for="school_id" :value="__('Escuela profesional')" />
+            <select name="school_id" id="school_id" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                <option value="">-- Seleccione --</option>
+                @foreach ($schools as $school)
+                    <option value="{{$school->id}}" {{($school->id == old('school_id') ? 'selected' : null)}}>{{$school->name}}</option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('school_id')" class="mt-2" />
+        </div>
+
+        <!--Condition -->
+        <div class="mt-4">
+            <x-input-label for="condition" :value="__('Condición')" />
+            <select name="condition" id="condition" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                <option value="">-- Seleccione --</option>
+                <option value="estudiante" {{("estudiante" == old('condition') ? 'selected' : null)}}>Estudiante</option>
+                <option value="egresado" {{("egresado" == old('condition') ? 'selected' : null)}}>Egresado</option>
+            </select>
+            <x-input-error :messages="$errors->get('condition')" class="mt-2" />
         </div>
 
         <!-- Password -->
