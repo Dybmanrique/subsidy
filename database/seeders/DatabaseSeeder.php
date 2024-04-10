82a04454c22d64dcf7ce94c659726b37b5332097
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-10 10:23:56 -0500

    Corrigiendo el seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Deyber Manrique',
+            'name' => 'Deyber',
+            'last_name' => 'Manrique',
             'email' => 'dmanriquea@unasam.edu.pe',
         ]);
     }
