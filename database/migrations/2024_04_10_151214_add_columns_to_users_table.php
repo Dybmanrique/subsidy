918819e1529a11c4c87fadddea8a336d4c5f7663
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-10 16:54:42 -0500

    Agregando columna de es admin a los usuarios

--- database/migrations/2024_04_10_151214_add_columns_to_users_table.php
+++ database/migrations/2024_04_10_151214_add_columns_to_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('last_name');
+            $table->boolean('is_admin')->default(false);
         });
     }
 };
