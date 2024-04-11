6e2110db83f1e114fcd93dd460cd214cf56b2e81
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-11 18:32:50 -0500

    Agregando sweet alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,7 @@
                 {{ $slot }}
             </main>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         @stack('scripts')
     </body>
 </html>
