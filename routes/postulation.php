e7939075ea8ced7d0df00c1df6114fb134182ae0
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-14 03:00:52 -0500

    Agregando la ruta para subir los requerimientos

--- routes/postulation.php
+++ routes/postulation.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Postulation\PostulationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostulationController::class, 'index'])->middleware(['auth', 'verified'])->name('postulations.index');
+Route::get('/mis-postulaciones/{postulation}/editar', [PostulationController::class, 'postulate'])->middleware(['auth', 'verified'])->name('postulations.postulate');
