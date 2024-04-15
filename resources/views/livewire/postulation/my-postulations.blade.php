fd3c5afb5f4b728bf0f9b3fda23b22beb0bbb62c
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-14 21:27:31 -0500

    Mostrando el boton de edición según el estado

--- resources/views/livewire/postulation/my-postulations.blade.php
+++ resources/views/livewire/postulation/my-postulations.blade.php
@@ -5,13 +5,20 @@
             @foreach ($postulations as $postulation)
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-2">
                     <div class="p-6 text-gray-900">
-                        <p><span class="font-bold">Nombre: </span>{{$postulation->name ?? 'Sin nombre'}}</p>
-                        <p><span class="font-bold">Asesor: </span>{{$postulation->adviser ?? 'Sin asesor'}}</p>
-                        <p><span class="font-bold">Estado: </span>{{$postulation->status}}</p>
-                        <p><span class="font-bold">Convocatoria: </span>{{$postulation->announcement->name}}</p>
-                        <p><span class="font-bold">Tipo de subvención: </span>{{$postulation->announcement->subsidy->name}}</p>
+                        <p><span class="font-bold">Nombre: </span>{{ $postulation->name ?? 'Sin nombre' }}</p>
+                        <p><span class="font-bold">Asesor: </span>{{ $postulation->adviser ?? 'Sin asesor' }}</p>
+                        <p><span class="font-bold">Estado: </span>{{ $postulation->status }}</p>
+                        <p><span class="font-bold">Convocatoria: </span>{{ $postulation->announcement->name }}</p>
+                        <p><span class="font-bold">Tipo de subvención:
+                            </span>{{ $postulation->announcement->subsidy->name }}</p>
                         <div class="mt-2">
-                            <x-primary-link href="{{route('postulations.postulate', $postulation->id)}}">Editar</x-primary-link>
+                            @if (
+                                $postulation->status !== 'Pendiente de revisión' &&
+                                    $postulation->status !== 'Denegado en la Dirección de Investigación e Innovación')
+                                <x-primary-link
+                                    href="{{ route('postulations.postulate', $postulation->id) }}">Editar</x-primary-link>
+                            @endif
+
                             <x-primary-button>Generar Solicitud</x-primary-button>
                             <x-primary-button>Ver documentos</x-primary-button>
                         </div>
