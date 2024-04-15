51f381df67dc6860cdfa164b3be47af681c6bdab
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-14 20:56:59 -0500

    Agregando la ruta de mis postulaciones a las rutas

--- routes/postulation.php
+++ routes/postulation.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\Postulation\PostulationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostulationController::class, 'index'])->middleware(['auth', 'verified'])->name('postulations.index');
+Route::get('/mis-postulaciones', [PostulationController::class, 'my_postulations'])->middleware(['auth', 'verified'])->name('postulations.my_postulations');
 Route::get('/mis-postulaciones/{postulation}/editar', [PostulationController::class, 'postulate'])->middleware(['auth', 'verified'])->name('postulations.postulate');
