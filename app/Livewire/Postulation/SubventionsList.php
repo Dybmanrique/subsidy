e95516fe775ef68471cef5221f72d00ab672a6de
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-16 01:39:00 -0500

    Cambiando el modal para los atributos de la postulación

--- app/Livewire/Postulation/SubventionsList.php
+++ app/Livewire/Postulation/SubventionsList.php
@@ -5,21 +5,40 @@ namespace App\Livewire\Postulation;
 use App\Models\Postulation;
 use App\Models\Subsidy;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class SubventionsList extends Component
 {
     public $subsidies;
+    public $model_open = false;
+    public $activity_items = [];
+
+    public $name, $activity_id, $adviser, $subsidy_id;
 
     public function mount(){
         $this->subsidies = Subsidy::where('status','activo')->get();
     }
 
-    public function postulate($subsidy_id, $subsidy_name){
-        $subsidy = Subsidy::find($subsidy_id);
+    public function selectSubsidy($subsidy_id){
+        $this->subsidy_id = $subsidy_id;
+        $this->activity_items = Subsidy::find($subsidy_id)->activities()->where('status','activo')->get();
+    }
+
+    public function postulate(){
+        $this->validate([
+            'name' => 'required|string|max:255',
+            'adviser' => 'nullable|string|max:255',
+            'activity_id' => 'required|numeric',
+        ]);
+
+        $subsidy = Subsidy::find($this->subsidy_id);
         $announcement = $subsidy->announcement()->latest()->first();
         
         $postulation = Postulation::create([
-            'name' => $subsidy_name,
+            'name' => $this->name,
+            'adviser' => $this->adviser,
+            'uuid' => Str::uuid()->toString(),
+            'activity_id' => $this->activity_id,
             'student_id' => auth()->user()->student->id,
             'announcement_id' => $announcement->id,
         ]);
--- resources/views/livewire/postulation/subventions-list.blade.php
+++ resources/views/livewire/postulation/subventions-list.blade.php
@@ -1,54 +1,91 @@
-<div>
+<div x-data="{ modelOpen: $wire.entangle('model_open') }">
+
+    <div>
+        <div x-show="modelOpen" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog"
+            aria-modal="true">
+            <div class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
+                <div x-cloak @click="modelOpen = false" x-show="modelOpen"
+                    x-transition:enter="transition ease-out duration-300 transform" x-transition:enter-start="opacity-0"
+                    x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-200 transform"
+                    x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
+                    class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true"></div>
+
+                <div x-cloak x-show="modelOpen" x-transition:enter="transition ease-out duration-300 transform"
+                    x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                    x-transition:leave="transition ease-in duration-200 transform"
+                    x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                    x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    class="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
+                    <div class="flex items-center justify-between space-x-4">
+                        <h1 class="text-xl font-medium text-gray-800 ">Postular a la subvención</h1>
+
+                        <button @click="modelOpen = false" class="text-gray-600 focus:outline-none hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </button>
+                    </div>
+
+                    <form class="mt-5" wire:submit='postulate'>
+                        <div>
+                            
+
+                            <label for="user name" class="mt-3 block text-gray-700 capitalize">Actividad*:</label>
+                            <input placeholder="Nombre de la actividad" type="text" wire:model='name'
+                                class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+
+
+                            <label for="user name" class="mt-3 block text-gray-700 capitalize">Tipo*:</label>
+                            <select name="" id="" wire:model='activity_id'
+                                class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                <option value="">-- Seleccione --</option>
+                                @foreach ($activity_items as $activity)
+                                <option value="{{$activity->id}}">{{$activity->name}}</option>
+                                @endforeach
+                            </select>
+                            <x-input-error :messages="$errors->get('activity_id')" class="mt-2" />
+
+                            <label for="user name" class="mt-3 block text-gray-700 capitalize">Asesor:</label>
+                            <input placeholder="Nombres y apellidos del asesor" type="text" wire:model='adviser'
+                                class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                            <x-input-error :messages="$errors->get('adviser')" class="mt-2" />
+                        </div>
+
+                        <div class="flex justify-end mt-6">
+                            <x-primary-button>POSTULAR</x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
             @foreach ($subsidies as $subsidy)
                 <x-postulation.subvention-card
-                    img="https://unasam.edu.pe/web/noticiaunasam/noticia-04-04-2023-11-24-23.png"
                     title="{{ $subsidy->name }}">
                     <div class="whitespace-pre-wrap mb-2">{{ $subsidy->description }}</div>
-                    <x-primary-button onclick="postulate({{ $subsidy->id }})">
+                    <button
+                        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                        @click="modelOpen =!modelOpen" 
+                        wire:click = 'selectSubsidy({{ $subsidy->id }})'
+                        >
                         Postular
-                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            fill="none" viewBox="0 0 14 10">
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
-                    </x-primary-button>
+                    </button>
                 </x-postulation.subvention-card>
             @endforeach
 
-
         </div>
     </div>
-    @push('scripts')
-        <script src="{{ asset('js/admin/message_forms.js') }}"></script>
-        <script>
-            function postulate(id) {
-                Swal.fire({
-                    title: '¿Esta seguro?',
-                    html: "<p><span class='font-bold'>IMPORTANTE:</span> Recuerde que se puede postular sólo a UNA subvención por año. Si tiene dudas puede leer el REGLAMENTO</p><br><p class='text-left font-bold'>Nombre de la actividad:<p>",
-                    input: 'text',
-                    inputAttributes: {
-                        autocapitalize: 'off',
-                        style: ' border-radius: 5px;'
-                    },
-                    showCancelButton: true,
-                    confirmButtonText: 'POSTULAR',
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    cancelButtonText: 'CANCELAR',
-                    showLoaderOnConfirm: true,
-                    preConfirm: (name) => {
-                        if (!name) {
-                            Swal.showValidationMessage('Por favor ingresa el nombre de la actividad');
-                            return name;
-                        }
-                        @this.postulate(id, name)
-                    },
-                    allowOutsideClick: () => !Swal.isLoading()
-                });
-            }
-        </script>
-    @endpush
 </div>
