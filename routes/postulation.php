e7c24d9c0455f3d3df17130026539be1306b0d93
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-16 18:51:58 -0500

    Quitando la ruta de pdf solicitud

--- routes/postulation.php
+++ routes/postulation.php
@@ -7,4 +7,3 @@ Route::get('/', [PostulationController::class, 'index'])->middleware(['auth', 'v
 Route::get('/mis-postulaciones', [PostulationController::class, 'my_postulations'])->middleware(['auth', 'verified'])->name('postulations.my_postulations');
 Route::get('/mis-postulaciones/{postulation}/editar', [PostulationController::class, 'postulate'])->middleware(['auth', 'verified'])->name('postulations.postulate');
 Route::get('/{postulation}/ver', [PostulationController::class, 'view_documents'])->name('postulations.view_documents');
-Route::post('/mis-postulaciones/{postulation}/generar-pdf', [PostulationController::class, 'generate_solicitude'])->middleware(['auth', 'verified'])->name('postulations.generate_solicitude');
