19ac0c93078e75adfac14b8f9479c94f8d5de221
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-16 01:39:14 -0500

    Actualizando la vista

--- resources/views/livewire/postulation/upload-documents.blade.php
+++ resources/views/livewire/postulation/upload-documents.blade.php
@@ -68,19 +68,19 @@
                             class="flex-column space-y space-y-0 text-sm font-medium text-gray-500 md:me-4 mb-4 md:mb-0">
                             @foreach ($requirements as $index => $requirement)
                                 <li>
-                                    <a href="#"
-                                        class="text-nowrap inline-flex items-center px-4 py-3 mb-1 rounded-lg bg-gray-50 hover:text-gray-100 hover:bg-blue-700 w-full"
+                                    <button
+                                        class="border shadow text-nowrap inline-flex items-center px-4 py-3 mb-1 rounded-lg bg-gray-50 hover:text-gray-100 hover:bg-blue-700 w-full"
                                         x-on:click="openTab = {{ $index }}"
-                                        :class="{ 'bg-blue-700 text-white': openTab === {{ $index }} }">
+                                        :class="{ 'bg-blue-600 text-white': openTab === {{ $index }} }">
                                         Requisito {{ $index + 1 }}
-                                    </a>
+                                    </button>
                                 </li>
                             @endforeach
                         </ul>
 
                         @foreach ($requirements as $index => $requirement)
                             <div x-show="openTab === {{ $index }}"
-                                class="transition-all duration-300 p-6 bg-gray-50 text-medium text-gray-500  rounded-lg w-full">
+                                class="border shadow transition-all duration-300 p-6 bg-gray-50 text-medium text-gray-500  rounded-lg w-full">
                                 <h3 class="text-lg font-bold text-gray-900  mb-2">{{ $requirement->name }}</h3>
                                 <p class="mb-2">{{ $requirement->description }}</p>
                                 <div class="flex justify-between">
