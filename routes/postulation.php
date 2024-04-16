e39d0a929fa1e99c404f78383a53224903d08d95
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-16 02:25:41 -0500

    Quitando la incogruencia de la url

--- routes/postulation.php
+++ routes/postulation.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostulationController::class, 'index'])->middleware(['auth', 'verified'])->name('postulations.index');
 Route::get('/mis-postulaciones', [PostulationController::class, 'my_postulations'])->middleware(['auth', 'verified'])->name('postulations.my_postulations');
 Route::get('/mis-postulaciones/{postulation}/editar', [PostulationController::class, 'postulate'])->middleware(['auth', 'verified'])->name('postulations.postulate');
-Route::get('/mis-postulaciones/{postulation}/ver', [PostulationController::class, 'view_documents'])->name('postulations.view_documents');
+Route::get('/{postulation}/ver', [PostulationController::class, 'view_documents'])->name('postulations.view_documents');
