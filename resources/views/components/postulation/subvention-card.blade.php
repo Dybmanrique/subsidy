bad50a6294c1cce8fc318bd2817518b81829d0c4
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-16 01:37:36 -0500

    Quitando codigo inncecesario

--- resources/views/components/postulation/subvention-card.blade.php
+++ resources/views/components/postulation/subvention-card.blade.php
@@ -1,9 +1,5 @@
 <div>
-      
     <div class="w-full my-1 bg-white border border-gray-200 rounded-lg shadow ">
-        <a href="#">
-            <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-        </a>
         <div class="p-5">
             <a href="#">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{{$title}}</h5>
