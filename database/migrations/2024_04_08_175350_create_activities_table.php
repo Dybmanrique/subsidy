5fbf6d58d62bc1948a58b63ffb72db905d553770
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-15 20:01:43 -0500

    Agregando el estado a las actividades

--- database/migrations/2024_04_08_175350_create_activities_table.php
+++ database/migrations/2024_04_08_175350_create_activities_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('status',['activo','inactivo'])->default('activo');
             $table->foreignId('subsidy_id')->constrained();
             $table->timestamps();
         });
