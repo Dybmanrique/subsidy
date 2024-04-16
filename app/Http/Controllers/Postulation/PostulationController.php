aeba1f98733d63e5b9b17738ca5602ab031cbf24
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-16 11:32:25 -0500

    Agregando el método para generar qr

--- app/Http/Controllers/Postulation/PostulationController.php
+++ app/Http/Controllers/Postulation/PostulationController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Postulation;
 use App\Http\Controllers\Controller;
 use App\Models\Postulation;
 use App\Models\Subsidy;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class PostulationController extends Controller
 {
@@ -24,5 +26,19 @@ class PostulationController extends Controller
 
     public function view_documents(Postulation $postulation) {
         return view('postulations.view_documents', compact('postulation'));
+    }
+
+    public function generate_solicitude(Postulation $postulation) {
+        $url = route('postulations.view_documents', $postulation);
+        $qrcode = base64_encode(QrCode::format('svg')->size(200)->errorCorrection('H')->generate($url));
+        $data = [
+            'postulation' => $postulation,
+            'user' => auth()->user(),
+            'qrcode' => $qrcode,
+            'url' => $url
+        ];
+        $pdf = Pdf::loadView('postulations.solicitude', $data);
+        return $pdf->stream();
+        // return view('postulations.solicitude', $data);
     }
 }
