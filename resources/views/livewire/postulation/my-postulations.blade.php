bc1db5d7d13ffd881515b02170881a90826f0c6d
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-16 23:53:45 -0500

    Agregando un texto más descriptivo

--- resources/views/livewire/postulation/my-postulations.blade.php
+++ resources/views/livewire/postulation/my-postulations.blade.php
@@ -30,7 +30,7 @@
 
                     <form class="mt-5" wire:submit='generateSolicitude()'>
                         <div>
-                            <label for="user name" class="mt-3 block text-gray-700 capitalize">Firma*:</label>
+                            <label for="user name" class="mt-3 block text-gray-700">Si desea puede agregar una imagen de su firma:</label>
                             <input wire:model = 'file_modal' accept="image/*"
                                 class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none"
                                 type="file" />
