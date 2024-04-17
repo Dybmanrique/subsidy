ecc34df9f818b4a4a428bdb3cf07edf9a9831ea9
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-16 23:41:39 -0500

    Asignando los colores a los botones

--- resources/views/livewire/postulation/my-postulations.blade.php
+++ resources/views/livewire/postulation/my-postulations.blade.php
@@ -71,9 +71,9 @@
                                 class="mb-1 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
                                 wire:click = 'setPostulationId({{ $postulation->id }})'
                                 @click="modelOpen =!modelOpen">Generar solicitud</button>
-                            <x-primary-link class="mb-1"
+                            <x-success-link class="mb-1"
                                 href="{{ route('postulations.view_documents', $postulation) }}" target="_blank">Ver
-                                documentos</x-primary-link>
+                                documentos</x-success-link>
                         </div>
                     </div>
                 </div>
