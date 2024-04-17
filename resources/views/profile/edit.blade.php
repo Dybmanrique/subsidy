2d592164a365df3237bcd81f10809ee7ad6d35b8
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-16 23:31:30 -0500

    corrigiendo color del header

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+        <h2 class="font-semibold text-xl text-gray-200 leading-tight">
+            {{ __('MI PERFIL') }}
         </h2>
     </x-slot>
 
