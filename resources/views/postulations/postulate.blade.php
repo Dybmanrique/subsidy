0db2b55930ecf901bfa93d40fc4bdd4225fdcc30
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-16 23:06:40 -0500

    Corrigiendo el color y el texto

--- resources/views/postulations/postulate.blade.php
+++ resources/views/postulations/postulate.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Subir documentos') }}
+        <h2 class="font-semibold text-xl text-gray-200 leading-tight">
+            {{ __('POR FAVOR SUBA TODOS LOS DOCUMENTOS NECESARIOS') }}
         </h2>
     </x-slot>
 
