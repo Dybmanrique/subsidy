00b2ff1484efe1fde50dcef355ae1e171fcf5b65
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-19 13:03:25 -0500

    Corrigiendo el color del focus

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
 $classes = ($active ?? false)
             ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-300 text-sm font-medium leading-5 text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-400 hover:text-gray-300 hover:border-indigo-400 focus:outline-none focus:text-gray-300 focus:border-gray-700 transition duration-150 ease-in-out';
+            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-400 hover:text-gray-300 hover:border-indigo-400 focus:outline-none focus:text-gray-300 focus:border-indigo-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
