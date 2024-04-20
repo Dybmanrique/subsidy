c2be23e78717c5b0548a116c49f19bc83aee0e5d
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-20 18:10:32 -0500

    Corrigiendo los métodos

--- app/Livewire/Admin/Postulations/FormActions.php
+++ app/Livewire/Admin/Postulations/FormActions.php
@@ -2,7 +2,8 @@
 
 namespace App\Livewire\Admin\Postulations;
 
-use App\Mail\PostulationDeniedMailable;
+use App\Mail\GeneralMessageMailable;
+use Exception;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
@@ -10,43 +11,77 @@ use Livewire\Component;
 class FormActions extends Component
 {
     public $postulation;
-    public $reason_for_denial;
+    public $title;
+    public $message;
+
+    public function sendMessage(){
+        try {
+            Mail::to($this->postulation->student->user->email)->send(new GeneralMessageMailable($this->title, $this->message));
+            $this->dispatch('message', code: '200', content: 'Hecho, se le envió el mensaje');
+            $this->reset(['title','message']);
+        } catch (Exception $ex) {
+            $this->dispatch('message', code: '500', content: 'Algo salió mal');
+        } finally {
+            $this->dispatch('close_modal');
+        }
+    }
 
     public function eliminar(){
-        // $this->postulation->status = 'Pendiente de revisión';
-        foreach ($this->postulation->requirements as $requirement) {
-            Storage::delete($requirement->pivot->file);
+        try {
+            foreach ($this->postulation->requirements as $requirement) {
+                Storage::delete($requirement->pivot->file);
+            }
+            $this->postulation->delete();
+            $this->dispatch('message', code: '200', content: 'Hecho');
+            return redirect()->route('admin.postulations.all_index', $this->postulation->announcement->subsidy->id);
+        } catch (Exception $ex) {
+            $this->dispatch('message', code: '500', content: 'Algo salió mal');
         }
-        $this->postulation->delete();
-        $this->dispatch('message', code: '200', content: 'Hecho');
-        return redirect()->route('admin.postulations.all_index', $this->postulation->announcement->subsidy->id);
     }
     public function aprobarDII(){
-        $this->postulation->status = 'Aceptado en la Dirección del Instituto de Investigación';
-        $this->postulation->save();
-        $this->dispatch('message', code: '200', content: 'Hecho');
+        try {
+            $this->postulation->status = 'Aceptado en la Dirección del Instituto de Investigación';
+            $this->postulation->save();
+            $this->dispatch('message', code: '200', content: 'Hecho');
+        } catch (Exception $ex) {
+            $this->dispatch('message', code: '500', content: 'Algo salió mal');
+        }
     }
     public function denegarDII(){
-        Mail::to($this->postulation->student->user->email)->send(new PostulationDeniedMailable($this->reason_for_denial));
-        $this->postulation->status = 'Denegado en la Dirección del Instituto de Investigación';
-        $this->postulation->save();
-        $this->dispatch('message', code: '200', content: 'Hecho, se le envió el mensaje');
-        $this->dispatch('close_modal');
+        try {
+            $this->postulation->status = 'Denegado en la Dirección del Instituto de Investigación';
+            $this->postulation->save();
+            $this->dispatch('message', code: '200', content: 'Hecho');
+        } catch (Exception $ex) {
+            $this->dispatch('message', code: '500', content: 'Algo salió mal');
+        }
     }
     public function aprobarConsejo(){
-        $this->postulation->status = 'Aprobado en el Consejo Universitario';
-        $this->postulation->save();
-        $this->dispatch('message', code: '200', content: 'Hecho');
+        try {
+            $this->postulation->status = 'Aprobado en el Consejo Universitario';
+            $this->postulation->save();
+            $this->dispatch('message', code: '200', content: 'Hecho');
+        } catch (Exception $ex) {
+            $this->dispatch('message', code: '500', content: 'Algo salió mal');
+        }
     }
     public function denegarConsejo(){
-        $this->postulation->status = 'Denegado en el Consejo Universitario';
-        $this->postulation->save();
-        $this->dispatch('message', code: '200', content: 'Hecho');
+        try {
+            $this->postulation->status = 'Denegado en el Consejo Universitario';
+            $this->postulation->save();
+            $this->dispatch('message', code: '200', content: 'Hecho');
+        } catch (Exception $ex) {
+            $this->dispatch('message', code: '500', content: 'Algo salió mal');
+        }
     }
     public function reafirmar(){
-        $this->postulation->status = 'Aceptado en la Dirección del Instituto de Investigación';
-        $this->postulation->save();
-        $this->dispatch('message', code: '200', content: 'Hecho');
+        try {
+            $this->postulation->status = 'Pendiente de revisión';
+            $this->postulation->save();
+            $this->dispatch('message', code: '200', content: 'Hecho');
+        } catch (Exception $ex) {
+            $this->dispatch('message', code: '500', content: 'Algo salió mal');
+        }
     }
 
     public function render()
