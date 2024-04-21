9b9dc5e5b7cf98942f8644c0158f627b88161c79
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-21 17:34:23 -0500

    Cambiando la url de las rutas

--- routes/postulation.php
+++ routes/postulation.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Postulation\PostulationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostulationController::class, 'index'])->middleware(['auth', 'verified'])->name('postulations.index');
+Route::get('/subvenciones', [PostulationController::class, 'index'])->middleware(['auth', 'verified'])->name('postulations.index');
 Route::get('/mis-postulaciones', [PostulationController::class, 'my_postulations'])->middleware(['auth', 'verified'])->name('postulations.my_postulations');
 Route::get('/mis-postulaciones/{postulation}/editar', [PostulationController::class, 'postulate'])->middleware(['auth', 'verified'])->name('postulations.postulate');
-Route::get('/{postulation}/ver', [PostulationController::class, 'view_documents'])->name('postulations.view_documents');
+Route::get('/postulaciones/{postulation}/ver', [PostulationController::class, 'view_documents'])->name('postulations.view_documents');
