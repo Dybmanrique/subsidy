4653319e5bbc32ff205f8600872c4df777077b52
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-21 13:03:45 -0500

    Agregando la tabla lista negra

--- database/migrations/2024_04_21_050219_create_black_list_table.php
+++ database/migrations/2024_04_21_050219_create_black_list_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('black_list', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->string('reason');
+            $table->text('reason');
             $table->timestamps();
         });
     }
