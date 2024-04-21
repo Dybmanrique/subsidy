9f7d8ab7f20a0177d39f044db314122cd05f7f57
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-21 17:35:33 -0500

    Creando los metodos para las ultimas postulaciones

--- app/Http/Controllers/Admin/PostulationController.php
+++ app/Http/Controllers/Admin/PostulationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Announcement;
 use App\Models\Postulation;
 use App\Models\Subsidy;
 use Illuminate\Http\Request;
@@ -26,23 +27,55 @@ class PostulationController extends Controller
             join users on user_id = users.id
             join schools on school_id = schools.id
             join faculties on faculty_id = faculties.id
-            where announcements.subsidy_id = $subsidy->id");
+            where announcements.subsidy_id = :subsidy_id order by postulations.id desc", ['subsidy_id' => $subsidy->id]);
     }
 
     public function view_postulation($postulation_id)
     {
         $postulation = Postulation::findOrFail($postulation_id);
-        
-        $previous_id = DB::scalar("SELECT postulations.id from postulations
-        join announcements on announcement_id = announcements.id
-        where announcements.subsidy_id = :subsidy_id and postulations.id < :postulation_id order by id desc limit 1", 
-        ['subsidy_id' => $postulation->announcement->subsidy->id, 'postulation_id' => $postulation->id]);
-        
-        $next_id = DB::scalar("SELECT postulations.id from postulations
-        join announcements on announcement_id = announcements.id
-        where announcements.subsidy_id = :subsidy_id and postulations.id > :postulation_id order by id asc limit 1", 
-        ['subsidy_id' => $postulation->announcement->subsidy->id, 'postulation_id' => $postulation->id]);
 
-        return view('admin.postulations.view_postulation', compact('postulation','previous_id','next_id'));
+        $previous_id = DB::scalar(
+            "SELECT postulations.id from postulations
+        join announcements on announcement_id = announcements.id
+        where announcements.subsidy_id = :subsidy_id and postulations.id < :postulation_id order by id desc limit 1",
+            ['subsidy_id' => $postulation->announcement->subsidy->id, 'postulation_id' => $postulation->id]
+        );
+
+        $next_id = DB::scalar(
+            "SELECT postulations.id from postulations
+        join announcements on announcement_id = announcements.id
+        where announcements.subsidy_id = :subsidy_id and postulations.id > :postulation_id order by id asc limit 1",
+            ['subsidy_id' => $postulation->announcement->subsidy->id, 'postulation_id' => $postulation->id]
+        );
+
+        return view('admin.postulations.view_postulation', compact('postulation', 'previous_id', 'next_id'));
+    }
+
+    public function last_index(Subsidy $subsidy)
+    {
+        return view('admin.postulations.last_index', compact('subsidy'));
+    }
+
+    public function last_data(Subsidy $subsidy)
+    {
+        $last_announcement = Announcement::where('subsidy_id', $subsidy->id)->orderByDesc('id')->first();
+
+        if ($last_announcement) {
+            return DB::select(
+                "SELECT postulations.id, postulations.name as postulation, postulations.status,
+            announcements.name as announcement, 
+            users.name, users.last_name, users.email, schools.name as school, faculties.name as faculty
+            from postulations
+            join students on student_id = students.id
+            join announcements on announcement_id = announcements.id
+            join users on user_id = users.id
+            join schools on school_id = schools.id
+            join faculties on faculty_id = faculties.id
+            where announcements.subsidy_id = :subsidy_id and announcements.id = :announcement_id order by postulations.id desc",
+                ['subsidy_id' => $subsidy->id, 'announcement_id' => $last_announcement->id]
+            );
+        } else {
+            return [];
+        }
     }
 }
