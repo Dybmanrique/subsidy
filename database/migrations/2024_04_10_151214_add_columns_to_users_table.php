745a488992db6bc2c8e84661b6cbfc4502dc4465
Deyber Manrique <dbrmanrique@gmail.com>
2024-04-22 00:22:32 -0500

    agregando la columna de estado a los usuarios

--- database/migrations/2024_04_10_151214_add_columns_to_users_table.php
+++ database/migrations/2024_04_10_151214_add_columns_to_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('last_name');
+            $table->enum('status',['activo','inactivo'])->default('activo');
         });
     }
 
