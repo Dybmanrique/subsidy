9a79f0f34e2ee57703719c1b71152a3003d6d6db
Deyber Manrique <dbrmanrique@gmail.com>
2024-05-02 00:12:12 -0500

    Modificando texto del dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'UNASAM')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1 class="font-weight-bold">PANEL DE ADMINISTRACIÓN</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>Bienvenido al panel de administración de las subvenciones.</p>
 @stop
 
 @section('css')
