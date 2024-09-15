f0cabdd7f18c88c1fd8f341c268d07216abe5aac
Deyber Manrique <dbrmanrique@gmail.com>
2024-09-15 18:53:47 -0500

    Corrigiendo la relación de escuelas

--- database/migrations/2024_04_08_174747_create_schools_table.php
+++ database/migrations/2024_04_08_174747_create_schools_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('schools', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('faculty_id');
+            $table->foreignId('faculty_id')->constrained();
             $table->timestamps();
         });
     }
