9138f79748ab5ebc38cbe37b8a4b951f59b0554b
Deyber <dbrmanrique@gmail.com>
2024-09-27 17:10:44 -0500

    Agregando una descripción

--- database/migrations/2024_09_15_235152_create_postulation_state_table.php
+++ database/migrations/2024_09_15_235152_create_postulation_state_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('postulation_state', function (Blueprint $table) {
             $table->foreignId('postulation_id')->constrained()->onDelete('cascade');
             $table->foreignId('state_id')->constrained()->onDelete('cascade');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
