6375e029731900ee4d450f83e9dcfd1aa7e41087
Deyber <dbrmanrique@gmail.com>
2024-09-28 15:15:05 -0500

    Agregando el modelo para los parámetros

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Faculty;
 use App\Models\School;
+use App\Models\Seeting;
 use App\Models\State;
 use App\Models\Student;
 use App\Models\User;
@@ -36,7 +37,7 @@ class DatabaseSeeder extends Seeder
 
         Faculty::create(['name' => 'Ciencias']);
         School::create(['name' => 'Sistemas', 'faculty_id' => 1]);
-        Vicerrector::create(['name'=>'Teresa', 'last_name'=>'Vicerrectora']);
+        Vicerrector::create(['name' => 'Teresa', 'last_name' => 'Vicerrectora']);
 
         User::factory()->create([
             'name' => 'Antonio',
@@ -51,5 +52,11 @@ class DatabaseSeeder extends Seeder
             'user_id' => '2',
             'condition' => '1',
         ]);
+
+        Seeting::create(['key' => 'max_postulation', 'value' => '1']);
+        Seeting::create(['key' => 'regulation_link', 'value' => 'https://investigacion.unasam.edu.pe/noticia/reglamento-subvenciones-economicas-a-favor-de-graduados-y-estudiantes-de-pregrado-para-el-desarrollo-de-actividades-academicas-y-de-investigacion-formativa-unasam-2024']);
+        Seeting::create(['key' => 'cover_image', 'value' => 'https://www.unasam.edu.pe/web/noticiaunasam/noticia-11-04-2024-09-12-51.jpg']);
+        Seeting::create(['key' => 'unasam_link', 'value' => 'https://unasam.edu.pe/']);
+        Seeting::create(['key' => 'vicerrectorate_link', 'value' => 'https://investigacion.unasam.edu.pe/']);
     }
 }
