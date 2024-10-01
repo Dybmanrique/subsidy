feab1af8d5292a0a63d132185a79446224ee08e8
Deyber <dbrmanrique@gmail.com>
2024-09-30 21:39:21 -0500

    Agregando filtros

--- resources/views/admin/postulations/all_index.blade.php
+++ resources/views/admin/postulations/all_index.blade.php
@@ -15,6 +15,19 @@
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-sm w-100 my-2" id="table">
+                    <tfoot>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
                     <thead class="thead-dark">
                         <tr>
                             <th scope="col">#</th>
@@ -41,7 +54,11 @@
 @stop
 
 @section('css')
-
+    <style>
+        tfoot {
+            display: table-header-group;
+        }
+    </style>
 @stop
 
 @section('js')
@@ -114,7 +131,83 @@
                     url: 'https://cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json'
                 },
                 columnDefs: columnDefs,
-                responsive: true
+                // responsive: true,
+                buttons: [{
+                        extend: 'copy',
+                        footer: false,
+                        exportOptions: {
+                            columns: [0, 2, 3, 4, 5, 6, 7, 8]
+                        }
+                    },
+                    {
+                        extend: 'excel',
+                        footer: false,
+                        exportOptions: {
+                            columns: [0, 2, 3, 4, 5, 6, 7, 8]
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        footer: false,
+                        exportOptions: {
+                            columns: [0, 2, 3, 4, 5, 6, 7, 8]
+                        }
+                    },
+                ],
+                layout: {
+                    topEnd: 'buttons'
+                },
+                initComplete: function() {
+                    this.api()
+                        .columns([2, 4, 5])
+                        .every(function() {
+                            let column = this;
+                            let title = column.footer().textContent;
+
+                            // Create input element
+                            let input = document.createElement('input');
+                            input.classList.add('form-control')
+                            input.placeholder = title;
+                            column.footer().replaceChildren(input);
+
+                            // Event listener for user input
+                            input.addEventListener('keyup', () => {
+                                if (column.search() !== this.value) {
+                                    column.search(input.value).draw();
+                                }
+                            });
+                        });
+
+                    this.api()
+                        .columns([3, 6, 7, 8])
+                        .every(function() {
+                            let column = this;
+
+                            // Create select element
+                            let select = document.createElement('select');
+                            select.add(new Option(''));
+                            select.classList.add('form-control')
+                            column.footer().replaceChildren(select);
+
+                            // Apply listener for user change in value
+                            select.addEventListener('change', function() {
+                                column
+                                    .search(select.value, {
+                                        exact: true
+                                    })
+                                    .draw();
+                            });
+
+                            // Add list of options
+                            column
+                                .data()
+                                .unique()
+                                .sort()
+                                .each(function(d, j) {
+                                    select.add(new Option(d));
+                                });
+                        });
+                }
             });
         });
     </script>
