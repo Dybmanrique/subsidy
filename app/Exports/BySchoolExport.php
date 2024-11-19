87bb4ed638d056774e2a4aa541c8fff610ad396b
Deyber <dbrmanrique@gmail.com>
2024-11-19 16:39:47 -0500

    Cambiando la manera de consulta

--- app/Exports/BySchoolExport.php
+++ app/Exports/BySchoolExport.php
@@ -13,8 +13,8 @@ class BySchoolExport implements FromView
     private $year;
     private $activity;
     private $faculty;
-    private $total_students=0;
-    private $total_budget=0;
+    private $total_students = 0;
+    private $total_budget = 0;
 
     public function __construct(int $year, Activity $activity, Faculty $faculty)
     {
@@ -25,19 +25,26 @@ class BySchoolExport implements FromView
 
     public function view(): View
     {
-        $records = Postulation::selectRaw('SUM(student_members) as total_students, SUM(graduated_members) as total_graduates, SUM(budget) as total_budget, schools.name as school')
-            ->join('students', 'student_id', '=', 'students.id')
-            ->join('schools', 'schools.id', '=', 'students.school_id')
-            ->join('faculties', 'faculties.id', '=', 'faculty_id')
+        $records = Postulation::selectRaw('
+        SUM(student_members) as total_students, 
+        SUM(graduated_members) as total_graduates, 
+        SUM(budget) as total_budget, 
+        schools.name as school
+    ')
+            ->join('students', 'postulations.student_id', '=', 'students.id')
+            ->join('schools', 'students.school_id', '=', 'schools.id')
+            ->join('faculties', 'schools.faculty_id', '=', 'faculties.id')
             ->join('postulation_state', 'postulation_state.postulation_id', '=', 'postulations.id')
-            ->where('state_id', 5)
-            ->where('activity_id', $this->activity->id)
-            ->where('faculties.id', $this->faculty->id)
-            ->groupBy('school_id')
+            ->where([
+                ['postulation_state.state_id', '=', 5],
+                ['faculties.id', '=', $this->faculty->id],
+                ['postulations.activity_id', '=', $this->activity->id],
+            ])
             ->whereYear('postulations.created_at', $this->year)
-            ->get();
+            ->groupBy('schools.id', 'schools.name')
+            ->get();;
 
-        
+
 
         foreach ($records as $record) {
             $this->total_students += ($record->total_students + $record->total_graduates);
@@ -45,8 +52,8 @@ class BySchoolExport implements FromView
         }
 
         return view('admin.reports.by-school', [
-            'records' => $records, 
-            'year' => $this->year, 
+            'records' => $records,
+            'year' => $this->year,
             'activity' => $this->activity,
             'faculty' => $this->faculty,
             'total_students' => $this->total_students,
